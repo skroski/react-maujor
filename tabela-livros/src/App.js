0e231e0f45c1518a3582f7a8f65a7db2482d3521
daniel.skroski <daniel@duplod.com.br>
2021-10-12 18:25:57 -0300

    Atualização do Primeiro Projeto de React do Livro do Maujor

--- tabela-livros/src/App.js
+++ tabela-livros/src/App.js
@@ -8,21 +8,49 @@ class App extends Component {
   state = {
     livros: []
   }
-  componentDidMount(){
+  componentDidMount() {
     fetch("/api/livros.json")
-    .then(response => response.json())
-    .then(livros => this.setState({livros}))
-    .catch(function(error){
-      console.log("Erro na Requisição")
-    })
-    
+      .then(response => response.json())
+      .then(livros => this.setState({ livros }))
+      .catch(function (error) {
+        console.log("Erro na Requisição")
+      })
+      .finally(function () {
+        console.log("Sempre Retorna");
+      }
+
+      )
+
+  }
+  handleRemoverLinha = (id) => {
+    const livros = this.state.livros.filter(l => l.id !== id);
+    this.setState({ livros })
+
+  }
+  handleOrdenarCrescente = (titulo) => {
+    const livros = this.state.livros.sort((a, b) =>
+      a.titulo < b.titulo ? -1 : 0
+    )
+    this.setState({ livros })
+  }
+  handleOrdenarDecrescente = (titulo) => {
+    const livros = this.state.livros.sort((a, b) =>
+      a.titulo < b.titulo ? -1 : 0
+    )
+    livros.reverse();
+    this.setState({ livros })
   }
   render() {
     return (
       <table className="tabela">
-        <TabelaHead />
-        <TabelaFooter qdeLivros = {this.state.livros.length} />
-        <TabelaBody livros={this.state.livros} />
+        <TabelaHead
+          ordenarCrescente={this.handleOrdenarCrescente}
+          ordenarDecrescente={this.handleOrdenarDecrescente}
+        />
+        <TabelaFooter qdeLivros={this.state.livros.length} />
+        <TabelaBody livros={this.state.livros}
+          removerLinha={this.handleRemoverLinha}
+        />
       </table>
     );
   }
--- tabela-livros/src/components/TabelaBody.js
+++ tabela-livros/src/components/TabelaBody.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TabelaBody = props => (
+const TabelaBody = (props) => (
     <tbody>
 
         {props.livros.map((livro, index) => (
@@ -9,7 +9,11 @@ const TabelaBody = props => (
                 <td>{livro.titulo}</td>
                 <td>{livro.autor}</td>
                 <td>
-                    <button className="botao remover">Remover</button>
+                    <button 
+                    className="botao remover" 
+                    onClick={() => props.removerLinha(livro.id)}
+                    id={livro.id}
+                    >Remover</button>
                 </td>
             </tr>
         ))}
